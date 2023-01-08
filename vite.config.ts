a5f8d98124441bf3f62964750c5cadcb7bae50ee
Ben Lu <ayroblu@gmail.com>
2023-01-08 15:25:49 +0000

    Try disabling navigation fallback

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,14 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: { navigateFallback: null },
+      // devOptions: {
+      //   enabled: true,
+      // },
+    }),
+  ],
 });
