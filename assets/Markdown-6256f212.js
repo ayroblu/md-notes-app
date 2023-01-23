3bf90bb010a33bb5d7144c88c9782b9916a3a698
ayroblu <ayroblu@users.noreply.github.com>
2023-01-23 09:54:13 +0000

    deploy: 0d4590f27163bfffba36efdc7613d1dcc8dd3982

---
+++ assets/Markdown-6256f212.js
@@ -0,0 +1,1 @@
+import{R as t,j as o,c as e,b as s,m as c}from"./index-14df3880.js";const r="_markdown_el777_1",d={markdown:r},w=t.memo(({className:n,text:a})=>o("div",{className:e(d.markdown,n),children:o(m,{text:a})}));function m({text:n}){return s(c(n))}export{w as Markdown,m as MyReactMarkdown,w as default};
